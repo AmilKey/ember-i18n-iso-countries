4548342d735b07b90a5534818d406b14c0c99182
Kobzar Ivan <kobzar.ivan1@gmail.com>
2016-04-11 12:20:12 +0300

    init commit

---
+++ addon/index.js
@@ -0,0 +1,192 @@
+import { CODES } from './codes';
+import EN from './langs/en';
+
+const langs = {
+  "en" : EN,
+};
+
+
+/*
+ * All codes map to ISO 3166-1 alpha-2
+ */
+const alpha2 = {};
+const alpha3 = {};
+const numeric = {};
+const invertedNumeric = {};
+
+CODES.forEach((codeInformation)=> {
+  const s = codeInformation;
+  alpha2[s[0]] = s[1];
+  alpha3[s[1]] = s[0];
+  numeric[parseInt(s[2], 10)] = s[0];
+  invertedNumeric[s[0]] = parseInt(s[2], 10);
+});
+
+/*
+ * @param code Alpha-3 code
+ * @return Alpha-2 code or undefined
+ */
+export function alpha3ToAlpha2(code) {
+  return alpha3[code];
+}
+// exports.alpha3ToAlpha2 = alpha3ToAlpha2;
+
+/*
+ * @param code Alpha-2 code
+ * @return Alpha-3 code or undefined
+ */
+export function alpha2ToAlpha3(code) {
+  return alpha2[code];
+}
+// exports.alpha2ToAlpha3 = alpha2ToAlpha3;
+
+/*
+ * @param code Alpha-3 code
+ * @return Numeric code or undefined
+ */
+export function alpha3ToNumeric(code) {
+  return invertedNumeric[alpha3ToAlpha2(code)];
+}
+// exports.alpha3ToNumeric = alpha3ToNumeric;
+
+/*
+ * @param code Alpha-2 code
+ * @return Numeric code or undefined
+ */
+export function alpha2ToNumeric(code) {
+  return invertedNumeric[code];
+}
+// exports.alpha2ToNumeric = alpha2ToNumeric;
+
+/*
+ * @param code Numeric code
+ * @return Alpha-3 code or undefined
+ */
+export function numericToAlpha3(code) {
+  return alpha2ToAlpha3(numeric[parseInt(code, 10)]);
+}
+// exports.numericToAlpha3 = numericToAlpha3;
+
+/*
+ * @param code Numeric code
+ * @return Alpha-2 code or undefined
+ */
+export function numericToAlpha2(code) {
+  return numeric[parseInt(code, 10)];
+}
+// exports.numericToAlpha2 = numericToAlpha2;
+
+/*
+ * @param code ISO 3166-1 alpha-2, alpha-3 or numeric code
+ * @return ISO 3166-1 alpha-3
+ */
+export function toAlpha3(code) {
+  if (typeof code === "string") {
+    if (/^[0-9]*$/.test(code)) {
+      return numericToAlpha3(code);
+    }
+    if(code.length === 2) {
+      return alpha2ToAlpha3(code.toUpperCase());
+    }
+    if (code.length === 3) {
+      return code.toUpperCase();
+    }
+  }
+  if (typeof code === "number") {
+    return numericToAlpha3(code);
+  }
+  return undefined;
+}
+// exports.toAlpha3 = toAlpha3;
+
+/*
+ * @param code ISO 3166-1 alpha-2, alpha-3 or numeric code
+ * @return ISO 3166-1 alpha-2
+ */
+export function toAlpha2(code) {
+  if (typeof code === "string") {
+    if (/^[0-9]*$/.test(code)) {
+      return numericToAlpha2(code);
+    }
+    if (code.length === 2) {
+      return code.toUpperCase();
+    }
+    if(code.length === 3) {
+      return alpha3ToAlpha2(code.toUpperCase());
+    }
+  }
+  if (typeof code === "number") {
+    return numericToAlpha2(code);
+  }
+  return undefined;
+}
+// exports.toAlpha2 = toAlpha2;
+
+/*
+ * @param code ISO 3166-1 alpha-2, alpha-3 or numeric code
+ * @param lang language for country name
+ * @return name or undefined
+ */
+export function getName(code, lang) {
+  try {
+    const l = langs[lang.toLowerCase()];
+    return l[toAlpha2(code)];
+  } catch (err) {
+    return undefined;
+  }
+};
+
+/*
+ * @param lang language for country name
+ * @return hash
+ */
+export function getNames(lang) {
+  try {
+    return langs[lang.toLowerCase()];
+  } catch (err) {
+    return {};
+  }
+};
+
+/*
+ * @param name name
+ * @param lang language for country name
+ * @return ISO 3166-1 alpha-2 or undefined
+ */
+export function getAlpha2Code(name, lang) {
+  try {
+    let p;
+    const codenames = langs[lang.toLowerCase()];
+    for (p in codenames) {
+      if (codenames.hasOwnProperty(p)) {
+        if (codenames[p].toLowerCase() === name.toLowerCase()) {
+          return p;
+        }
+      }
+    }
+    return undefined;
+  } catch (err) {
+    return undefined;
+  }
+};
+
+/*
+ * @return hash (alpha-2 => alpha-3)
+ */
+export function getAlpha2Codes() {
+  return alpha2;
+};
+
+/*
+ * @return hash (alpha-3 => alpha-2)
+ */
+export function getAlpha3Codes() {
+  return alpha3;
+};
+
+/*
+ * @return hash (numeric => alpha-2)
+ */
+export function getNumericCodes() {
+  return numeric;
+};
